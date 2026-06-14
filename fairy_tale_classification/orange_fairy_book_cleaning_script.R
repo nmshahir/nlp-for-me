25a60a7482cf393015e89d6512107af361e9d819
Nur M Shahir <nmshahir@proton.me>
2026-06-14 17:35:39 -0400

    Created initial cleaning scripts for orange, olive, lilac, and yellow fairy books

---
+++ fairy_tale_classification/orange_fairy_book_cleaning_script.R
@@ -0,0 +1,27 @@
+################################################################
+# Name: orange_fairy_book_cleaning_script.R
+# Purpose: Pulling Lang's Fairy Books from Project Gutenberg and
+#          and labeling them by story (i.e. chapter) within them
+#          Removes extraneous information such as Prefaces.
+# Creator: N. M. Shahir
+# Github: nmshahir
+# Data source: Project Gutenberg
+# Data Obtained: 2026.06.14
+# Date Created: 2026.06.14
+# Last Update: 2026.06.14
+#################################################################
+
+install.packages("tidytext")
+# install.packages("pak")
+pak::pak("ropensci/gutenbergr")
+
+library(tidytext)
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(gutenbergr)
+library(RColorBrewer)
+
+gutenberg_get_mirror()
+
+orange_fairy_book <- gutenberg_download(36532)
