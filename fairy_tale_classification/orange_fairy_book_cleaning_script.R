63a07ceb30abe2a3b1f727d6be82d739e309fd88
Nur M Shahir <nmshahir@proton.me>
2026-06-14 18:09:18 -0400

    Cleaning script for orange fairy book complete!

--- fairy_tale_classification/orange_fairy_book_cleaning_script.R
+++ fairy_tale_classification/orange_fairy_book_cleaning_script.R
@@ -25,3 +25,35 @@ library(RColorBrewer)
 gutenberg_get_mirror()
 
 orange_fairy_book <- gutenberg_download(36532)
+
+# Cleaning The Orange Fairy Book
+orange_chapter_titles <- orange_fairy_book |>
+  slice(212:276) |>
+  mutate(text = toupper(text)) |>
+  pull(text) |>
+  str_trim() |>
+  str_remove("\\s+\\d+$") |> # remove trailing page numbers
+  str_trim() |> # trim again after removals
+  str_subset("^.+$") # drop empty strings
+
+orange_regex <- orange_chapter_titles |>
+  str_escape() |>
+  str_c(collapse = "|") |>
+  (\(x) str_c("^\\s*(", x, ")\\s*$"))()
+
+orange_fairy_book_stories <- orange_fairy_book |>
+  gutenberg_add_sections(
+    pattern = orange_regex,
+    section_col = "story",
+    ignore_case = TRUE,
+    format_fn = str_trim
+  )
+
+orange_fairy_stories_only <- orange_fairy_book_stories |>
+  slice(426:11344) |>
+  mutate(story = str_replace_all(story, "_", "")) |> #Removes the underscores from story label
+  filter(!str_detect(text, "\\[Illustration:")) #removes the Illustration lines because evidently this version came with pictures??
+
+orange_fairy_stories_only |>
+  count(story) |>
+  print(n = 80)
